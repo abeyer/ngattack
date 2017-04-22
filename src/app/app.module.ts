345d74d8bb7bc93134466ea1ad37514f7f8bb2f6
Andrew Beyer <beyer.andrew@gmail.com>
2017-04-21 22:05:38 -0700

    hero-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroListComponent,
     HeroDetailComponent,
   ],
   imports: [
